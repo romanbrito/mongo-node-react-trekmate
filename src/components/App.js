0941b8ab0b019e12efd8c89dfd8d9607ae6827ff
Roman Brito <romanbrito1@gmail.com>
2017-11-08 13:57:21 -0600

    displaying list from json object

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Header from './Header';
+import ContestPreview from './ContestPreview';
 
 class App extends Component {
   state = {
@@ -16,7 +17,9 @@ class App extends Component {
       <div className="App">
         <Header message={this.state.pageHeader} />
         <div>
-          ---
+          {this.props.contests.map(contest =>
+            <ContestPreview {...contest } />
+          )}
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import data from './testData.json';
-console.log(data);
-
 import App from './components/App';
 
 ReactDOM.render(
-  <App />,
+  <App contests={data.contests}/>,
   document.getElementById('root')
 );
