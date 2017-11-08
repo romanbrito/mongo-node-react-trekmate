3a4a9235094a4d7f7cc7e2d4319a18cdcff3ad42
Roman Brito <romanbrito1@gmail.com>
2017-11-08 12:51:14 -0600

    Component state

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Header from './Header';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header message="Components with modules" />
-      <div>
-        ...
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { test: 42 };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header message="Components with modules" />
+        <div>
+          {this.state.test}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
