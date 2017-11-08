9fbc9d18d1c90cb95b2c3307de57d40986315543
Roman Brito <romanbrito1@gmail.com>
2017-11-08 08:29:56 -0600

    basic react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const color = Math.random() > 0.5 ? 'green' : 'red';
+
 ReactDOM.render(
-  React.createElement('h2', null, 'Hello React'),
+  <h2 style={{color: color}}>
+    Hello React with Handlebars!! {Math.random()}
+  </h2>,
   document.getElementById('root')
 );
