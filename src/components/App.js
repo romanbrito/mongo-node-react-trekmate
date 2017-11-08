00f4295b03748cb465c2a35a7c10a62d88249c04
Roman Brito <romanbrito1@gmail.com>
2017-11-08 13:16:46 -0600

    component life cycle

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,16 @@ class App extends Component {
   state = {
     pageHeader: 'Trekmate App'
   };
+  componentDidMount(){
+    console.log('did Mount');
+    debugger;
+    // ajax,  timers, listeners
+  }
+  componentWillUnmount () {
+    console.log('will Unmount');
+    debugger;
+    // clean timers, listeners
+  }
   render() {
     return (
       <div className="App">
