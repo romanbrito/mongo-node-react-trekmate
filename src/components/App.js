3c43a3b5c46cc1319d0014db9c7444c1afe96993
Roman Brito <romanbrito1@gmail.com>
2017-11-09 09:24:27 -0600

    changing url when clicking content

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,16 @@ import axios from 'axios';
 import Header from './Header';
 import ContestList from './ContestList';
 
+const pushState = (obj, url) =>  //browser history for html5
+  window.history.pushState(obj, '', url);
+
 class App extends Component {
   state = {
     pageHeader: 'Trekmate App',
     contests: this.props.initialContests
   };
-  componentDidMount(){
+
+  componentDidMount() {
     // ajax,  timers, listeners
     axios.get('/api/contests')
       .then(resp => {
@@ -18,14 +22,25 @@ class App extends Component {
       })
       .catch(console.error);
   }
-  componentWillUnmount () {
+
+  componentWillUnmount() {
     // clean timers, listeners
   }
+
+  fetchContest = (contestId) => {
+    pushState(
+      { currentContestId: contestId },
+      `/contest/${contestId}` // backtick template literals
+    );
+  };
+
   render() {
     return (
       <div className="App">
-        <Header message={this.state.pageHeader} />
-        <ContestList contests={this.state.contests} />
+        <Header message={this.state.pageHeader}/>
+        <ContestList
+          onContestClick={this.fetchContest}
+          contests={this.state.contests}/>
       </div>
     );
   }
