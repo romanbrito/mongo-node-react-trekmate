b56583cafa5a82ef3d4d374fd02b87d330f521ea
Roman Brito <romanbrito1@gmail.com>
2017-11-10 09:06:57 -0600

    removing test data to start working with mongodb

--- api/index.js
+++ api/index.js
@@ -1,22 +1,22 @@
 import express from 'express';
-import data from '../src/testData.json';
+//import data from '../src/testData.json';
 
 const router = express.Router();
-const contests = data.contests.reduce((obj, contest) => { // turning array into object for efficiency
-  obj[contest.id] = contest;
-  return obj;
-}, {});
+// const contests = data.contests.reduce((obj, contest) => { // turning array into object for efficiency
+//   obj[contest.id] = contest;
+//   return obj;
+// }, {});
 
 router.get('/contests', (req, res) => {
-  res.send({
-    contests: contests
-  });
+  // res.send({
+  //   contests: contests
+  // });
 });
 
 router.get('/contests/:contestId', (req, res) => { // dynamic contestId
-  let contest = contests[req.params.contestId];
-  contest.description = 'fake description since using fake data still';
-  res.send(contest);
+  // let contest = contests[req.params.contestId];
+  // contest.description = 'fake description since using fake data still';
+  // res.send(contest);
 });
 
 export default router;
