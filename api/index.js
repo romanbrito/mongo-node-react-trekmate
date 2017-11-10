38762068080d9a2a72ad42afec398e286254eaa0
Roman Brito <romanbrito1@gmail.com>
2017-11-10 15:41:00 -0600

    api to fetch list of names from mongodb

--- api/index.js
+++ api/index.js
@@ -35,6 +35,24 @@ router.get('/contests', (req, res) => {
     });
 });
 
+router.get('/names/:nameIds', (req, res) => { //http://localhost:8080/api/names/101,102
+  //mongodb native driver
+  const nameIds = req.params.nameIds.split(',').map(Number); //converts array of string into number [101,102]
+
+  let names = {}; //
+  mdb.collection('names').find({ id: { $in: nameIds }})
+    .each((err, name) => { //async so we cant res from each so workaround
+      assert.equal(null, err);
+
+      if (!name) { // no more contests
+        res.send( { names } ); // object
+        return;
+      }
+
+      names[name.id] = name;
+    });
+});
+
 router.get('/contests/:contestId', (req, res) => { // dynamic contestId
   mdb.collection('contests')
     .findOne({id: Number(req.params.contestId)})
