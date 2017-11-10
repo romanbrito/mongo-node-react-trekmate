67fc31cf3675867324df3e392fa9fb0272e24441
Roman Brito <romanbrito1@gmail.com>
2017-11-10 09:47:53 -0600

    converted contests to object by adding {} and added api/contest/:contestId to respond from database

--- api/index.js
+++ api/index.js
@@ -18,11 +18,16 @@ router.get('/contests', (req, res) => {
   //mongodb native driver
   let contests = {}; //
   mdb.collection('contests').find({})
+    .project({ // only the fields to be included
+      id: 1,
+      categoryName: 1,
+      contestName: 1
+    })
     .each((err, contest) => { //async so we cant res from each so workaround
       assert.equal(null, err);
 
       if (!contest) { // no more contests
-        res.send(contests);
+        res.send( { contests } ); // object
         return;
       }
 
@@ -31,7 +36,10 @@ router.get('/contests', (req, res) => {
 });
 
 router.get('/contests/:contestId', (req, res) => { // dynamic contestId
-
+  mdb.collection('contests')
+    .findOne({id: Number(req.params.contestId)})
+    .then(contest => res.send(contest))
+    .catch(console.error);
 });
 
 export default router;
