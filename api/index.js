8c4098f63e4b705748b58497e797cbee85f67b3f
Roman Brito <romanbrito1@gmail.com>
2017-11-11 20:29:56 -0600

    api that receives a post request and saves new name into collection and return value with findAndModify

--- api/index.js
+++ api/index.js
@@ -56,7 +56,33 @@ router.get('/contests/:contestId', (req, res) => { // dynamic contestId
   mdb.collection('contests')
     .findOne({_id: ObjectID(req.params.contestId)})
     .then(contest => res.send(contest))
-    .catch(console.error);
+    .catch(error => {
+      console.error(error);
+      res.status(404).send('Bad request');
+    });
+});
+
+router.post('/names', (req, res) => {
+  const contestId = ObjectID(req.body.contestId);
+  const name = req.body.newName;
+  // validation...
+  mdb.collection('names').insertOne({ name }).then(result =>
+    mdb.collection('contests').findAndModify(
+      { _id: contestId },
+      [],
+      { $push: { nameIds: result.insertedId}},
+      { new: true }
+    ). then(doc =>
+      res.send({
+        updatedContest: doc.value,
+        newName: { _id: result.insertedId, name }
+      })
+    )
+  )
+    .catch(error => {
+      console.error(error);
+      res.status(404).send('Bad request');
+    });
 });
 
 export default router;
